02a959c116ffa5ba0a4f0bdc0c5278185d02700f
Suyog Acharya <asuyog042@gmail.com>
2024-11-26 15:59:33 +0545

    unauthorized uploading method

--- client/src/components/Unauthenticated.jsx
+++ client/src/components/Unauthenticated.jsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { ThreeDots } from "react-loader-spinner";
 
 const Unauthenticated = () => {
-  return (
-    <div>Unauthenticated</div>
-  )
-}
+  const [img, setImg] = useState(null);
+  const [video, setVideo] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-export default Unauthenticated
+  const navigate = useNavigate();
+
+  const uploadFile = async (type) => {
+    const data = new FormData();
+    data.append("file", type === "image" ? img : video);
+    data.append(
+      "upload_preset",
+      type === "image" ? "images_preset" : "videos_preset"
+    );
+
+    try {
+        const cloudName = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+        let resourceType = type === "image" ? "image" : "video";
+      let api = `https://api.cloudinary.com/v1_1/${cloudName}/${resourceType}/upload`;
+
+      const res = await axios.post(api, data);
+      const { secure_url } = res.data;
+      console.log(secure_url);
+      return secure_url;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+
+      // Upload image file
+      const imgUrl = await uploadFile("image");
+
+      // Upload video file
+      const videoUrl = await uploadFile("video");
+
+      // Send backend api request
+      //await axios.post(`${process.env.REACT_APP_BACKEND_BASEURL}/api/videos`, { imgUrl, videoUrl });
+
+      // Reset states
+      setImg(null);
+      setVideo(null);
+
+      console.log("File upload success!");
+      setLoading(false);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="video">Video:</label>
+          <br />
+          <input
+            type="file"
+            accept="video/*"
+            id="video"
+            onChange={(e) => setVideo((prev) => e.target.files[0])}
+          />
+        </div>
+        <br />
+        <div>
+          <label htmlFor="img">Image:</label>
+          <br />
+          <input
+            type="file"
+            accept="image/*"
+            id="img"
+            onChange={(e) => setImg((prev) => e.target.files[0])}
+          />
+        </div>
+        <br />
+        <button type="submit">Upload</button>
+      </form>
+
+      {loading && (
+        <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="#4fa94d"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={true}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Unauthenticated;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -24,7 +24,5 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <RouterProvider router={router} />
 );
